10216cd16b2122a58341dbe297eff05a829e89c7
otadev <gospodin777@mail.ru>
2026-03-17 10:22:26 +0500

    исправили ошибки

--- cart/models.py
+++ cart/models.py
@@ -69,6 +69,6 @@ class CartItem(models.Model):
 
     @property
     def total_price(self):
-        return Decimal(str(self.product.price)*self.quantity)
+        return Decimal(str(self.product.price))*self.quantity
 
 
